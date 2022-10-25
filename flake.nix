6b1d47357def688f6ab9d6149efd933e9214305f
aalbert <augustin.albert@bleu-azure.fr>
2022-10-25 15:19:45 +0200

    feat(flake): add flake

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, flake-utils, nixpkgs }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        python = pkgs.python310;
+        packages = pkgs.python310Packages;
+      in {
+        devShells.default = pkgs.mkShell {
+          name = "blm-reaction-dev";
+          packages = [ python packages.matplotlib packages.numpy ];
+        };
+      });
+}
