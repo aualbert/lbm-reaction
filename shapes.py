03bf08f1b7deaae6e35955305595b32b45bd3329
Illia Bovtriuk <il.bovtruk@gmail.com>
2022-11-13 10:22:10 +0100

    drawwalls

--- shapes.py
+++ shapes.py
@@ -1,5 +1,6 @@
 import random
-
+import imageio
+import numpy as np
 def circle(X, Y, x, y, r):
     return (X - x) ** 2 + (Y - y) ** 2 < r**2
 
@@ -10,22 +11,13 @@ def rectangle(X, Y, x, y, xl, yl):
     return ((X - x) ** 2 < (xl / 2) ** 2) & ((Y - y) ** 2 < (yl / 2) ** 2)
 
 def ellipse(X, Y, x, y, a, b, r):
-    return (X - x) ** 2 / a ** 2 + (Y - y) ** 2 / b ** 2 < r**2
+    return (X - x) ** 2 / a ** 2 + (Y - y) ** 2 / b ** 2 < r ** 2
 
-
-def randfig(X, Y, x, y, Nx, Ny):
-    #state = random.randint(0, 3)
-    state = 3
-    if state == 0:
-        return circle(X, Y, x, y, r)
-    elif state == 1:
-        div = random.randint(2,6)
-        return square(X,Y,x,y, min(Nx, Ny) / div)
-    elif state == 2:
-        divx = random.randint(2, 6)
-        divy = random.randint(2,6)
-        return rectangle(X,Y,x,y,Nx / divx, Ny / divy)
-    elif state == 3:
-        a = random.randint(2,4)
-        b = random.randint(1,4)
-        return ellipse(X,Y,x,y,a,b , 3 * (a ** 2 + b ** 2))
+def test(imagename):
+    im=imageio.imread(imagename)
+    answ = np.full(X.shape, False)
+    for i in range(X.shape[0]):
+        for j in range(X.shape[1]):
+            if not np.all(im[i][j]):
+                answ[i][j] = True
+    return answ
