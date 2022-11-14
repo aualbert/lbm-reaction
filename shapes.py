d0a95a6640c255076fb7734d90060f954d5c9984
Illia Bovtriuk <il.bovtruk@gmail.com>
2022-11-14 15:39:45 +0100

    images_and_nutrients

--- shapes.py
+++ shapes.py
@@ -13,11 +13,32 @@ def rectangle(X, Y, x, y, xl, yl):
 def ellipse(X, Y, x, y, a, b, r):
     return (X - x) ** 2 / a ** 2 + (Y - y) ** 2 / b ** 2 < r ** 2
 
-def importimage(imagename):
+
+#black is border
+def import_border(imagename, x, y):
     im = imageio.imread(imagename)
+    im = im[:y,:x,:]
     answ = np.full(im.shape[:2], False)
     for i in range(im.shape[0]):
         for j in range(im.shape[1]):
             if not np.all(im[i][j]):
                 answ[i][j] = True
+    return answ
+
+#maps colours to nutrients but i am not sure if this mapping is good
+def colour_mapper(colour):
+    return int(colour[0]) + int(colour[1]) + int(colour[2])
+
+#any colour but black and white is nutrients. The witer the color is(the closer to 255/255/255) the more nutriens there are
+def set_nutrients(imagename, x, y):
+    im = imageio.imread(imagename)
+    im=im[:y,:x,:]
+    answ = np.zeros((im.shape[0], im.shape[1], 9))
+    for i in range(im.shape[0]):
+        for j in range(im.shape[1]):
+            #if color is black than ignore it as it is border
+            colour_sum = colour_mapper(im[i][j])
+            if colour_sum == 765:
+                continue
+            answ[i][j][8] = colour_sum
     return answ
